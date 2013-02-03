3959104d09c675096e8c350952d5f7a4515d24c3
Nathan Holt <NathanRandallHolt@gmail.com>
2013-02-03 13:24:48 -0700

    Adding a file to store a set of buttons for configurable contols

---
+++ ArdorCraftAPI/src/com/ardorcraft/control/ButtonSet.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.ardorcraft.control;
+
+import com.ardor3d.input.Key;
+import com.ardor3d.input.MouseButton;
+import java.util.ArrayList;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+
+/**
+ * @author Nathan
+ *
+ *  A set of keys or mouse buttons to map to a function
+ *  for creating configurable controls.
+ */
+public class ButtonSet
+{
+    @XmlElementWrapper(name="Keys")
+    @XmlElement(name="Key")
+    public ArrayList<Key> keys;
+    
+    @XmlElementWrapper(name="MouseButtons")
+    @XmlElement(name="Button")
+    public ArrayList<MouseButton> mouseButtons;
+}
