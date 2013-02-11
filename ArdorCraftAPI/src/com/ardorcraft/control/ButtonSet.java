44dccd4240e89446bd069a492d1417fc570eefbc
Nathan Randall Holt <NathanRandallHolt@gmail.com>
2013-02-10 22:04:15 -0700

    Replaced ArrayLists with TreeSets and added constructors for creating unions of buttonsets.

--- ArdorCraftAPI/src/com/ardorcraft/control/ButtonSet.java
+++ ArdorCraftAPI/src/com/ardorcraft/control/ButtonSet.java
@@ -5,7 +5,9 @@ package com.ardorcraft.control;
 
 import com.ardor3d.input.Key;
 import com.ardor3d.input.MouseButton;
-import java.util.ArrayList;
+
+import java.util.Collection;
+import java.util.TreeSet;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
@@ -18,13 +20,63 @@ import javax.xml.bind.annotation.XmlElementWrapper;
  */
 public class ButtonSet
 {
+    /**
+     * The set of keys in the set.
+     */
     @XmlElementWrapper(name="Keys")
     @XmlElement(name="Key")
-    public ArrayList<Key> keys =
-        new ArrayList<Key>();
+    public TreeSet<Key> keys;
     
+    /**
+     * The set of mouse buttons in the set.
+     */
     @XmlElementWrapper(name="MouseButtons")
     @XmlElement(name="Button")
-    public ArrayList<MouseButton> mouseButtons =
-        new ArrayList<MouseButton>();
+    public TreeSet<MouseButton> mouseButtons;
+    
+    /**
+     * Default constructor just creates
+     * an empty set.
+     */
+    public ButtonSet()
+    {
+        keys = new TreeSet<Key>();
+        mouseButtons = new TreeSet<MouseButton>();
+    }
+    
+    /**
+     * Create a union of a number of
+     * button sets.
+     * @param buttonSets
+     *      The buttonsets to combine
+     */
+    public ButtonSet(ButtonSet...buttonSets)
+    {
+        keys = new TreeSet<Key>();
+        mouseButtons = new TreeSet<MouseButton>();
+        
+        for(ButtonSet set : buttonSets)
+        {
+            keys.addAll(set.keys);
+            mouseButtons.addAll(set.mouseButtons);
+        }
+    }
+    
+    /**
+     * Constructs a set containing the
+     * union of the sets passed in.
+     * @param buttonSets
+     *      The sets to combine.
+     */
+    public ButtonSet(Collection<ButtonSet> buttonSets)
+    {
+        keys = new TreeSet<Key>();
+        mouseButtons = new TreeSet<MouseButton>();
+        
+        for(ButtonSet set : buttonSets)
+        {
+            keys.addAll(set.keys);
+            mouseButtons.addAll(set.mouseButtons);
+        }
+    }
 }
